02c34ff24777ebe885c3e0a519947ff9f201da78
Furqan Ramadhan <furqan21@mhs.unsyiah.ac.id>
2025-03-28 00:20:07 +0700

    add dosenwalimanager

---
+++ DosenWaliManager.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class DosenWaliManager {
+    private Dosen dosen;
+    private ArrayList<Mahasiswa> perwalian;
+
+    public DosenWaliManager(Dosen dosen, ArrayList<Mahasiswa> perwalian) {
+        this.dosen = dosen;
+        this.perwalian = perwalian;
+    }
+
+    //TODO : Implements in interface add later
+}
--- MahasiswaManager.java
+++ MahasiswaManager.java
@@ -4,4 +4,6 @@ public class MahasiswaManager {
     public MahasiswaManager(Mahasiswa mahasiswa) {
         this.dataMahasiswa = mahasiswa;
     }
+
+    //TODO : Add in interface
 }
