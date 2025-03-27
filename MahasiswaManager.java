e0037e94eb0cb6cdf7d39356f5afe07b9b057ba1
Furqan Ramadhan <furqan21@mhs.unsyiah.ac.id>
2025-03-28 00:10:18 +0700

    MahasiswaManager remove get set

--- MahasiswaManager.java
+++ MahasiswaManager.java
@@ -1,17 +1,7 @@
 public class MahasiswaManager {
     private Mahasiswa dataMahasiswa;
-
     // Constructor
     public MahasiswaManager(Mahasiswa mahasiswa) {
-        this.dataMahasiswa = mahasiswa;
-    }
-
-    // Getter and Setter
-    public Mahasiswa getMahasiswa() {
-        return dataMahasiswa;
-    }
-
-    public void setMahasiswa(Mahasiswa mahasiswa) {
         this.dataMahasiswa = mahasiswa;
     }
 }
