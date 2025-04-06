f0989e5f161578cc5adb972913e8cf8791922a19
toosakarin1412 <toosakarin1412@gmail.com>
2025-04-06 13:59:56 +0700

    feat: update todo dosen wali manager

--- DosenWaliManager.java
+++ DosenWaliManager.java
@@ -2,11 +2,12 @@ import java.util.ArrayList;
 
 public class DosenWaliManager implements IDosenWali {
     private Dosen dosen;
-    private ArrayList<Mahasiswa> perwalian;
+    private ArrayList<String> perwalian;
+    IListPerwalian listPerwalian = new PerwalianManager();
 
     public DosenWaliManager(Dosen dosen, ArrayList<Mahasiswa> perwalian) {
         this.dosen = dosen;
-        this.perwalian = perwalian;
+        this.perwalian = listPerwalian.getListPerwalian(dosen);
     }
 
     @Override
