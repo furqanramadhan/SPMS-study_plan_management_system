a97a7e6a1ce35cc498f3b095cdfde82a83ed5dab
toosakarin1412 <toosakarin1412@gmail.com>
2025-04-06 12:56:03 +0700

    fix loop on spms

--- SPMS.java
+++ SPMS.java
@@ -21,44 +21,48 @@ public class SPMS{
     }
 
     private void selectRole(){
-        System.out.println("Select your role: ");
-        System.out.println("1. Admin");
-        System.out.println("2. Mahasiswa");
-        System.out.println("3. Dosen");
-        System.out.println("4. Exit");
-        System.out.print("Your choice: ");
-        int choice = scanner.nextInt();
-        scanner.nextLine(); // Consume newline
+        
+        while (true) {
+            System.out.println("Select your role: ");
+            System.out.println("1. Admin");
+            System.out.println("2. Mahasiswa");
+            System.out.println("3. Dosen");
+            System.out.println("4. Exit");
+            System.out.print("Your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
 
-        switch(choice){
-            case 1:
-                setAdminManager(new AdminManager());
-                adminManager.showMenu();
-                break;
-            case 2:
-                System.out.print("Input NPM: ");
-                String npm = scanner.nextLine();
-                Mahasiswa mahasiswa = mahasiswaManager.getMahasiswa(npm);
-                if (mahasiswa == null) {
-                    System.out.println("Mahasiswa not found.");
-                    return;
-                }
-                krsMahasiswaManager = new KRSMahasiswaManager(mahasiswa);
-                krsMahasiswaManager.showMenu();
-                break;
-            case 3:
-                // System.out.print("Input NIP: ");
-                // String nidn = scanner.nextLine();
-                // Dosen dosen = dosenManager.getDosen(nidn);
-                // dosenWaliManager = new DosenWaliManager(dosen);
-                // dosenWaliManager.showMenu();
-                break;
-            case 4:
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Invalid choice");
-                break;
+            switch(choice){
+                case 1:
+                    setAdminManager(new AdminManager());
+                    adminManager.showMenu();
+                    break;
+                case 2:
+                    System.out.print("Input NPM: ");
+                    String npm = scanner.nextLine();
+                    Mahasiswa mahasiswa = mahasiswaManager.getMahasiswa(npm);
+                    if (mahasiswa == null) {
+                        System.out.println("Mahasiswa not found.");
+                        return;
+                    }
+                    // TODO: Tambahkan Selamat data + nama mhs dan npm
+                    krsMahasiswaManager = new KRSMahasiswaManager(mahasiswa);
+                    krsMahasiswaManager.showMenu();
+                    break;
+                case 3:
+                    // System.out.print("Input NIP: ");
+                    // String nidn = scanner.nextLine();
+                    // Dosen dosen = dosenManager.getDosen(nidn);
+                    // dosenWaliManager = new DosenWaliManager(dosen);
+                    // dosenWaliManager.showMenu();
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
         }
     }
 
